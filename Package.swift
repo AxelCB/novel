02beaa34991e1324539c990172d37d32045fd970
Axel Collard Bovy <axcbovy@gmail.com>
2018-01-20 18:33:08 -0300

    Updated Package.swift to Swift4 declaration of packages

--- Package.swift
+++ Package.swift
@@ -1,27 +1,28 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
   name: "Novel",
-  targets: [
-    Target(name: "Demo", dependencies: ["NovelCore", "NovelAdmin", "NovelAPI", "NovelTheme"]),
-    Target(name: "NovelTheme", dependencies: ["NovelCore"]),
-    Target(name: "NovelAPI", dependencies: ["NovelCore"]),
-    Target(name: "NovelAdmin", dependencies: ["NovelCore"]),
-    Target(name: "NovelCore"),
+  products: [
+      .executable(name: "Demo", targets: ["Demo"]),
+      .library(name: "NovelTheme", targets: ["NovelTheme"]),
+      .library(name: "NovelAPI", targets: ["NovelAPI"]),
+      .library(name: "NovelAdmin", targets: ["NovelAdmin"]),
+      .library(name: "NovelCore", targets:["NovelCore"]),
   ],
   dependencies: [
-    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-    .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
-    .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
-    .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2),
-    .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
-    .Package(url: "https://github.com/vapor/validation-provider.git", majorVersion: 1)
+      .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.0.0")),
+      .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.0.0")),
+      .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.0.0")),
+      .package(url: "https://github.com/vapor-community/postgresql-provider.git", .upToNextMajor(from: "2.0.0")),
+      .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.0.0")),
+      .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.0.0"))
   ],
-  exclude: [
-    "Config",
-    "Database",
-    "Localization",
-    "Public",
-    "Resources"
+  targets: [
+      .target(name: "Demo", dependencies: ["NovelCore", "NovelAdmin", "NovelAPI", "NovelTheme"]),
+      .target(name: "NovelTheme", dependencies: ["NovelCore"], exclude: ["Config","Database","Localization","Public","Resources"]),
+      .target(name: "NovelAPI", dependencies: ["NovelCore"], exclude: ["Config","Database","Localization","Public","Resources"]),
+      .target(name: "NovelAdmin", dependencies: ["NovelCore"], exclude: ["Config","Database","Localization","Public","Resources"]),
+      .target(name: "NovelCore", dependencies: ["Vapor", "LeafProvider", "PostgreSQLProvider", "ValidationProvider", "AuthProvider"], exclude: ["Config","Database","Localization","Public","Resources"]),
   ]
 )
